8853b4c3d516e042699459f2511a4af2f79e04f5
BuildTools <unconfigured@null.spigotmc.org>
2023-04-07 12:09:51 -0600

    navbar working

---
+++ src/routes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../components/Navbar'
+import Video from '../components/Video'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+
+        </div>
+    );
+};
+
+export default Home;
